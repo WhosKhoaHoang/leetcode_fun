393467d33efc70975dc86c7be3f09525ab88db8c
Khoa Hoang <whoskhoahoang@gmail.com>
2020-04-18 13:02:12 +0900

    Do problem 1281

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.py
@@ -0,0 +1,25 @@
+
+
+class Solution:
+
+    # Runtime: 24 ms
+    # Memory: 13.8 MB
+    def subtractProductAndSum(self, n: int) -> int:
+        mult = 1
+        add = 0
+        while n > 0:
+            mult *= n % 10
+            add += n % 10
+            n //= 10
+
+        return (mult-add)
+
+
+if __name__ == "__main__":
+    # THINK:
+    # . How to get the count of digits? Use a while loop that "shaves" n
+    # . How to get the actual digits themselves? With integer division (//)
+    sol = Solution()
+    n = 234
+    result = sol.subtractProductAndSum(n)
+    print(result)
