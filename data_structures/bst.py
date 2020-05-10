7babf09c0ed1184b92b2c78d44e8f98694a3b298
Khoa Hoang <whoskhoahoang@gmail.com>
2020-05-10 21:23:35 +0900

    Define get_bst_list() for BST implementation

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -23,6 +23,8 @@ class BST:
         rtype: None
         """
         self.num_nodes += 1
+        # Edge case where there are
+        # no nodes in the tree
         if self.num_nodes == 1:
             self.root = node
         else:
@@ -41,6 +43,26 @@ class BST:
             setattr(prev_node, side, node)
 
 
+    def get_bst_list(self):
+        """
+        Gets a representation of this BST as a list
+        where the elements are the values of the nodes
+        and where the order of the elements within this list
+        corresponds to the order of the nodes at each level.
+        return: A list that represents this BST
+        rtype: [values of BST's nodes]
+        """
+        queue = [self.root]
+        result = []
+        # BFS traversal:
+        while queue:
+            cur_node = queue.pop(0)
+            if cur_node:
+                result.append(cur_node.val)
+                queue.extend([cur_node.left, cur_node.right])
+
+        return result
+
 
 class TreeNode:
     """
@@ -57,6 +79,16 @@ class TreeNode:
 
 
 
-
 if __name__ == "__main__":
-    pass
+    # Visual Representation of what I'm gonna make:
+    #             10
+    #          /      \
+    #        5         15
+    #       / \      /   \
+    #     3    7   null  18
+    bst = BST()
+    tree_vals = [10,5,15,3,7,18]
+    for val in tree_vals:
+        bst.insert(TreeNode(val))
+
+    print(bst.get_bst_list())
